276ecc2c48fda70e36e7e8e4800167395c648206
Inho Park <park.inho@gmail.com>
2018-10-31 15:20:26 +0900

    TIME_ZONE setting STATIC_ROOT setting

--- kone/settings.py
+++ kone/settings.py
@@ -106,7 +106,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+# TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'    # ihpark
 
 USE_I18N = True
 
@@ -119,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')      # ihpark
